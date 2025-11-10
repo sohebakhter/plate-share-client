d7b447ba02e2b5b3cc32d29dab050ca728d7587d
sohebakhter <sohebakhterbadhan@gmailcom>
2025-11-11 00:29:02 +0600

    added foodcard

--- src/Components/AvailableFoods.jsx
+++ src/Components/AvailableFoods.jsx
@@ -1,13 +1,12 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router";
-import useAuth from "../Hooks/useAuth"; 
+
 import useAxiosSecure from "../Hooks/useAxiosSecure";
+import FoodCard from "./FoodCard";
 
 const AvailableFoods = () => {
   const [foods, setFoods] = useState([]);
   const [loading, setLoading] = useState(true);
-  const navigate = useNavigate();
-  const { user } = useAuth();
+
   const axiosSecure = useAxiosSecure();
 
   useEffect(() => {
@@ -21,14 +20,9 @@ const AvailableFoods = () => {
       });
   }, [axiosSecure]);
 
-  const handleViewDetails = (id) => {
-    if (!user) {
-      return navigate("/login");
-    }
-    navigate(`/food/${id}`);
-  };
+  
 
-    if (loading) {
+  if (loading) {
     return (
       <div className="flex justify-center items-center h-[60vh]">
         <div className="animate-spin rounded-full h-16 w-16 border-b-2 border-blue-500"></div>
@@ -44,50 +38,7 @@ const AvailableFoods = () => {
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {foods.map((food) => (
-          <div
-            key={food._id}
-            className="bg-white shadow-md rounded-xl overflow-hidden border hover:shadow-lg transition"
-          >
-            <img
-              src={food.foodImage}
-              alt={food.foodName}
-              className="h-52 w-full object-cover"
-            />
-
-            <div className="p-4">
-              <h3 className="text-xl font-semibold text-gray-800">
-                {food.foodName}
-              </h3>
-
-              <div className="flex items-center gap-2 my-2">
-                <img
-                  src={food.donorImage}
-                  alt={food.donorName}
-                  className="w-8 h-8 rounded-full object-cover border"
-                />
-                <p className="text-sm text-gray-600">{food.donorName}</p>
-              </div>
-
-              <p className="text-gray-700 text-sm">
-                <span className="font-semibold">Serves:</span> {food.foodQuantity}
-              </p>
-              <p className="text-gray-700 text-sm">
-                <span className="font-semibold">Location:</span>{" "}
-                {food.pickupLocation}
-              </p>
-              <p className="text-gray-700 text-sm">
-                <span className="font-semibold">Expires:</span>{" "}
-                {food.expireDate}
-              </p>
-
-              <button
-                onClick={() => handleViewDetails(food._id)}
-                className="w-full mt-4 bg-green-600 hover:bg-green-700 text-white py-2 rounded-lg font-medium transition"
-              >
-                View Details
-              </button>
-            </div>
-          </div>
+          <FoodCard key={food._id} food={food}></FoodCard>
         ))}
       </div>
     </div>
---
+++ src/Components/FoodCard.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { useNavigate } from "react-router";
+import useAuth from "../Hooks/useAuth";
+
+const FoodCard = ({ food }) => {
+  const navigate = useNavigate();
+  const { user } = useAuth();
+
+  const handleViewDetails = (id) => {
+    if (!user) {
+      return navigate("/login");
+    }
+    navigate(`/food/${id}`);
+  };
+  return (
+    <div className="bg-white shadow-md rounded-xl overflow-hidden border hover:shadow-lg transition">
+      <img
+        src={food.foodImage}
+        alt={food.foodName}
+        className="h-52 w-full object-cover"
+      />
+
+      <div className="p-4">
+        <h3 className="text-xl font-semibold text-gray-800">{food.foodName}</h3>
+
+        <div className="flex items-center gap-2 my-2">
+          <img
+            src={food.donorImage}
+            alt={food.donorName}
+            className="w-8 h-8 rounded-full object-cover border"
+          />
+          <p className="text-sm text-gray-600">{food.donorName}</p>
+        </div>
+
+        <p className="text-gray-700 text-sm">
+          <span className="font-semibold">Serves:</span> {food.foodQuantity}
+        </p>
+        <p className="text-gray-700 text-sm">
+          <span className="font-semibold">Location:</span> {food.pickupLocation}
+        </p>
+        <p className="text-gray-700 text-sm">
+          <span className="font-semibold">Expires:</span> {food.expireDate}
+        </p>
+
+        <button
+          onClick={() => handleViewDetails(food._id)}
+          className="w-full mt-4 bg-green-600 hover:bg-green-700 text-white py-2 rounded-lg font-medium transition"
+        >
+          View Details
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default FoodCard;
